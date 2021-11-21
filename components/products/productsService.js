4e8ea28db1e92e096e7bd9c1f13c95da3743ee9a
Duc Ngo <trongduc1509@gmail.com>
2021-11-21 08:27:52 +0700

    testing

---
+++ components/products/productsService.js
@@ -0,0 +1,9 @@
+const express = require('mongoose');
+
+
+const router = express.Router();
+
+//Product Item Model
+const Product = require('../../models/schema/Product');
+
+module.exports = router;
