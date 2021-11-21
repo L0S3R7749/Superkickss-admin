d63700f1b14896165d07f769444d719c67cca6a0
caohaisil201 <caohaisil@gmail.com>
2021-11-21 09:05:41 +0700

    add form add product in temp.html

--- components/homepage/homepageController.js
+++ components/homepage/homepageController.js
@@ -1,3 +1,3 @@
 module.exports.home = (req,res,next)=>{
-    res.render('./homepage/index',{title: 'homepage', body: '../../views/dashboard/dashboard'});
+    res.render('./homepage/index',{title: 'Homepage', body: '../../views/dashboard/dashboard'});
 }
