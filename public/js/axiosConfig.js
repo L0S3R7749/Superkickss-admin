98110de167d51a0a05a08aef1d2da85dddc910bb
Duc Ngo <trongduc1509@gmail.com>
2021-12-07 19:53:14 +0700

    fix bug

--- public/js/axiosConfig.js
+++ public/js/axiosConfig.js
@@ -1,3 +1,3 @@
 // const API_URL = 'http://admin-superkickss.herokuapp.com';
-const API_URL = 'http://localhost:5000';
+const API_URL = 'http://admin-superkickss.herokuapp.com';
 module.exports = API_URL;
