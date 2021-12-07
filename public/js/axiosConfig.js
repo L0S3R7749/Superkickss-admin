53a6bd8d28e105307173fef2f118c2971259e988
caohaisil201 <caohaisil@gmail.com>
2021-12-07 19:16:08 +0700

    fix deploy link axios

--- public/js/axiosConfig.js
+++ public/js/axiosConfig.js
@@ -1,3 +1,3 @@
 // const API_URL = 'http://admin-superkickss.herokuapp.com';
-const API_URL = 'http://admin2-superkickss.herokuapp.com';
+const API_URL = 'http://admin-superkickss.herokuapp.com';
 module.exports = API_URL;
