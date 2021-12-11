e8cfcb3ecb530dbce957e12960f525e8a3315b81
caohaisil201 <caohaisil@gmail.com>
2021-12-11 16:23:51 +0700

    order table UI

--- components/orders/ordersController.js
+++ components/orders/ordersController.js
@@ -1,3 +1,18 @@
-module.exports.orders = (req,res,next)=>{
-    res.render('./homepage/index',{title: 'homepage', body: '../../views/orders/orders'});
-}
+// module.exports.orders = (req,res,next)=>{
+//     res.render('./homepage/index',{title: 'homepage', body: '../../views/orders/orders'});
+// }
+
+const express = require('express');
+const services = require('./ordersService');
+
+const router = express.Router();
+
+router.get("/",(req,res,next)=>{
+    res.render('./homepage/index',{title: 'Order', body: '../orders/orders'});
+})
+
+router.get("/detail",(req,res,next)=>{
+    res.render('./homepage/index',{title: 'Detail',body: '../orders/detail'});
+})
+
+module.exports = router;
