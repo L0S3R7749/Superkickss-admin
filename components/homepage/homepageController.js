7b38550fcf0d9584be415cf5c5daf07703fdd123
Truong Phu Hung <truongphuhung@gmail.com>
2022-01-17 01:42:03 +0700

    Frontend Top 10 best seller

--- components/homepage/homepageController.js
+++ components/homepage/homepageController.js
@@ -1,8 +1,18 @@
 const services = require("./homepageService");
 
-module.exports.home = async (req,res,next)=>{
-    const top_ten_best_seller = await services.top_ten_best_seller();
-    // console.log(top_ten_best_seller);
-
+module.exports.home = (req,res,next)=>{
     res.render('./homepage/index',{title: 'Superkickss-Admin', body: '../../views/dashboard/dashboard'});
+}
+
+module.exports.topten = async (req, res, next) => {
+    try {
+        const top_ten_best_seller = await services.top_ten_best_seller();
+        res.status(200).send({
+            top_ten_best_seller: top_ten_best_seller,
+        })
+    } catch (err) {
+        res.status(500).send({
+            message: err.message
+        })
+    }
 }
